9ce916ab94d6a4157b9cf3a759bcf1006e401c18
sanarat <sanaratk@scg.com>
2019-10-20 21:05:30 +0700

    fix warning

--- src/scg/findRestaurant.js
+++ src/scg/findRestaurant.js
@@ -1,4 +1,4 @@
-import config from '../config/localhost';
+import config from '../config';
 export default async function findRestaurant(req, res, next) {
     var request = require('request');
     try {
